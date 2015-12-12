a08a150e53760f507e0dcb21931de2b1c572be09
moxley <moxleystratton@househappy.org>
2015-12-12 14:20:20 -0800

    Adjust minitest addition_spec

--- minitest_sample/test/addition_spec.rb
+++ minitest_sample/test/addition_spec.rb
@@ -1,10 +1,8 @@
 require 'minitest/spec'
 require 'minitest/autorun'
-require "minitest/reporters"
-Minitest::Reporters.use! Minitest::Reporters::ANSI.new
 
 describe "Addition" do
-  it "addds" do
+  it "adds" do
     (1 + 1).must_equal 2
   end
 end
